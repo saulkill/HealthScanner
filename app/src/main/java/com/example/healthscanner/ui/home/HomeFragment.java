4caecb5e70813d982c76fb27bbcc2225ca9396cb
salkill <wjstk5128@gmail.com>
2021-11-01 17:25:48 +0900

    Initial commit

---
+++ app/src/main/java/com/example/healthscanner/ui/home/HomeFragment.java
@@ -0,0 +1,25 @@
+package com.example.healthscanner.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.healthscanner.R;
+import com.example.healthscanner.databinding.FragmentHomeBinding;
+
+public class HomeFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+}
---
+++ app/src/main/java/com/example/healthscanner/ui/notifications/NotificationsFragment.java
@@ -0,0 +1,24 @@
+package com.example.healthscanner.ui.notifications;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.healthscanner.R;
+import com.example.healthscanner.databinding.FragmentNotificationsBinding;
+
+public class NotificationsFragment extends Fragment {
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_notifications, container, false);
+    }
+}
